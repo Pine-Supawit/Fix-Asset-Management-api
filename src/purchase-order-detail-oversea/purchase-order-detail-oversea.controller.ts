753b947e755a31038ecc70a9a7cfe94666643edc
Tun <thampapon.tn@gmail.com>
2025-05-29 08:53:28 +0700

    :wrench: add apitags and rename api

--- src/purchase-order-detail-oversea/purchase-order-detail-oversea.controller.ts
+++ src/purchase-order-detail-oversea/purchase-order-detail-oversea.controller.ts
@@ -3,13 +3,14 @@ import { PurchaseOrderDetailOverseaService } from './purchase-order-detail-overs
 import { CreatePurchaseOrderDetailOverseaDto } from './dto/create-purchase-order-detail-oversea.dto';
 import { UpdatePurchaseOrderDetailOverseaDto } from './dto/update-purchase-order-detail-oversea.dto';
 import { NumberValidator } from '../utils/validation';
+import { ApiTags } from '@nestjs/swagger';
 
-
+@ApiTags('Purchase Order Detail Oversea')
 @Controller('purchase-order-detail-oversea')
 export class PurchaseOrderDetailOverseaController {
   constructor(private readonly purchaseOrderDetailOverseaService: PurchaseOrderDetailOverseaService) {}
 
-  @Get('purchaseOrderDetailOversea')
+  @Get()
   async purchaseOrderDetailOversea(@Query('poid') poid: Number, @Query('productId') productId: Number, @Query('prductno') productNo: Number) {
     const validPoid = NumberValidator(+poid);
     const validProductId = NumberValidator(+productId);
--- src/purchase-order-oversea/purchase-order-oversea.controller.ts
+++ src/purchase-order-oversea/purchase-order-oversea.controller.ts
@@ -13,22 +13,23 @@ import { PurchaseOrderOverseaService } from './purchase-order-oversea.service';
 import { CreatePurchaseOrderOverseaDto } from './dto/create-purchase-order-oversea.dto';
 import { UpdatePurchaseOrderOverseaDto } from './dto/update-purchase-order-oversea.dto';
 import { NumberValidator, TypeValidator } from '../utils/validation';
+import { ApiTags } from '@nestjs/swagger';
 
-
+@ApiTags('Purchase Order Oversea')
 @Controller('purchase-order-oversea')
 export class PurchaseOrderOverseaController {
   constructor(
     private readonly purchaseOrderOverseaService: PurchaseOrderOverseaService,
   ) {}
 
-  @Get('/purchaseOderOverseaList')
+  @Get('list')
   async purchaseOrderOverseaList(@Query('page') page: number) {
     const pageNum = NumberValidator(+page);
   
     return this.purchaseOrderOverseaService.purchaseOrderOverseaList(pageNum);
   }
 
-  @Get('/purchaseOrderOverseaByType')
+  @Get('type')
   async purchaseOrderOverseaByType(
     @Query('type') type: string,
     @Query('page') page: string,
